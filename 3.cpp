2d6c65b58cc54c6a35b79da99dd553966b549e13
masterSahil <7882mastersahil@gmail.com>
2026-06-16 17:49:05 +0530

    reversed

---
+++ 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reversed = 0;
+    cin >> n;
+
+    while (n != 0) {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n /= 10;
+    }
+
+    cout << reversed;
+
+    return 0;
+}
