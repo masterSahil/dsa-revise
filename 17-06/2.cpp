319614482f33bc356186fea6b47de7f6d475743d
masterSahil <7882mastersahil@gmail.com>
2026-06-17 16:49:36 +0530

    max and 2nd max

---
+++ 17-06/2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Enter the Size of Array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Please Enter the Array Elements: \n";
+    for(int i=0; i<n; i++)
+    {
+        cout << "Array [" << i << "]: ";
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    for(int i=0; i<n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    cout << "Max Element is " << max;
+}
---
+++ 17-06/3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Please Enter the Elements of Array: " << endl;
+    for(int i=0; i<n; i++)
+    {   
+        cout << "Array[" << i << "]: ";
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    for(int i=0; i<n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    int max2 = arr[0];
+    for(int i=0; i<n; i++)
+    {
+        if (arr[i] > max2 && arr[i] != max)
+        {
+            max2 = arr[i];
+            cout << "arr: " << arr[i] << endl;
+        }
+    }
+
+    cout << "Max :" << max << endl;
+    cout << "Max2 :" << max2 << endl;
+}
