aa82e1093504a72e0d9c5cf6a6f6665f542e4f9c
masterSahil <7882mastersahil@gmail.com>
2026-06-21 12:10:06 +0530

    armstrong

---
+++ 21-06/1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int temp = n;
+    int digits = 0;
+
+    while(temp) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+    int sum = 0;
+
+    while(temp) {
+        int digit = temp % 10;
+        sum += pow(digit, digits);
+        temp /= 10;
+    }
+
+    if(sum == n)
+        cout << "Armstrong";
+    else
+        cout << "Not Armstrong";
+}
