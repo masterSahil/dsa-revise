3be7b8b3c5fc4a13e88fa48d405cec65db5723f3
masterSahil <7882mastersahil@gmail.com>
2026-06-22 11:29:21 +0530

    find duplicate

---
+++ 22-06/3.cpp
@@ -0,0 +1,28 @@
+// find duplicate
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Enter Size of Array:";
+    cin >> n;
+
+    int array[n];
+    for(int i=0; i<n; i++)
+    {
+        cout << "Array[" << i <<"]: ";
+        cin >> n;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if (array[i] == array[j])
+            {
+                cout << "Array same elements: " << array[i] << endl;
+            }
+        }
+    }
+}
